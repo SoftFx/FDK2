c7dea5db359e28208ef94aeca8c5d7d55c22ea53
ikavalenka <ilya.kavalenka@soft-fx.lv>
2018-02-21 19:13:23 +0300

    ...

---
+++ Src/Common/ConnectException.cs
@@ -0,0 +1,15 @@
+﻿namespace TickTrader.FDK.Common
+{
+    using System;
+
+    public class ConnectException : Exception
+    {
+        public ConnectException()
+        {
+        }
+
+        public ConnectException(string text) : base(text)
+        {
+        }
+    }
+}
