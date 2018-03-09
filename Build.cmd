179d341742fe913a160b2f7ffd767024181611fc
ikavalenka <ilya.kavalenka@soft-fx.lv>
2018-03-09 15:19:22 +0300

    ...

---
+++ Build.cmd
@@ -0,0 +1,22 @@
+@echo off
+
+if [%1]==[] goto INVALID_COMMAND_LINE_ERROR
+
+cd  Src
+call Build.cmd || goto END
+
+cd ..\Samples
+call Build.cmd || goto END
+
+cd ..
+call Pack.cmd %1 || goto END
+
+del "TickTrader FDK "%1".zip"
+7z a -tzip "TickTrader FDK "%1".zip" "TickTrader FDK "%1"" || goto END
+
+goto END
+
+:INVALID_COMMAND_LINE_ERROR
+echo Error: Package version is not specified
+
+:END
---
+++ Samples/Build.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /t:Build /p:Configuration=Release
